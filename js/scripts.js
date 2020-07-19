d46cd117d5ec09eb8e28e157f8f71b95386cc022
humphrey-mutuma <humphreymutuma01@gmail.com>
2020-07-19 22:59:05 +0300

    Add a better user interface.

--- js/scripts.js
+++ js/scripts.js
@@ -46,56 +46,56 @@ function validateDays() {
 }
 // Main function
 function myFunction() {
-var CC,YY,MM,DD,gender;
-// Get the value of the input field fields
-CC = parseInt(document.getElementById("centuryInput").value);
-YY = parseInt(document.getElementById("yearInput").value );
-MM = parseInt(document.getElementById("monthInput").value);
-DD = parseInt( document.getElementById("dayInput").value );
-gender = document.querySelector('input[type=radio]:checked').value;
+    var CC,YY,MM,DD,gender;
+    // Get the value of the input field fields
+    CC = parseInt(document.getElementById("centuryInput").value);
+    YY = parseInt(document.getElementById("yearInput").value );
+    MM = parseInt(document.getElementById("monthInput").value);
+    DD = parseInt( document.getElementById("dayInput").value );
+    gender = document.querySelector('input[type=radio]:checked').value;
 
 //calculate the day of the week
- myDOW = Math.floor((((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7) ;
+    myDOW = Math.floor((((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7) ;
 
-document.getElementById("OutputDOW").innerHTML = myDOW ;
-document.getElementById("gender").innerHTML = gender ;
-// an array of male names
-var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-//an array of female names
-var female = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-//an array of days of the week
-var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"];
- 
-if(myDOW === 0 && gender=== "Male"){
-    document.getElementById("akanNameOutput").innerHTML = male[0] ;
-}else if(myDOW === 0 && gender === "Female"){
-    document.getElementById("akanNameOutput").innerHTML = female[0] ;
-}else if(myDOW === 1 && gender=== "Male"){
-    document.getElementById("akanNameOutput").innerHTML = male[1] ;
-}else if(myDOW === 1 && gender === "Female"){
-    document.getElementById("akanNameOutput").innerHTML = female[1] ;
-}else if(myDOW === 2 && gender=== "Male"){
-    document.getElementById("akanNameOutput").innerHTML = male[2] ;
-}else if(myDOW === 2 && gender === "Female"){
-    document.getElementById("akanNameOutput").innerHTML = female[2] ;
-}else if(myDOW === 3 && gender=== "Male"){
-    document.getElementById("akanNameOutput").innerHTML = male[3] ;
-}else if(myDOW === 3 && gender === "Female"){
-    document.getElementById("akanNameOutput").innerHTML = female[3] ;
-}else if(myDOW === 4 && gender=== "Male"){
-    document.getElementById("akanNameOutput").innerHTML = male[4] ;
-}else if(myDOW === 4 && gender === "Female"){
-    document.getElementById("akanNameOutput").innerHTML = female[4] ;
-}else if(myDOW === 5 && gender=== "Male"){
-    document.getElementById("akanNameOutput").innerHTML = male[5] ;
-}else if(myDOW === 5 && gender === "Female"){
-    document.getElementById("akanNameOutput").innerHTML = female[5] ;
-}else if(myDOW === 6 && gender=== "Male"){
-    document.getElementById("akanNameOutput").innerHTML = male[6] ;
-}else if(myDOW === 6 && gender === "Female"){
-    document.getElementById("akanNameOutput").innerHTML = female[6] ;
-}else{
-    document.getElementById("akanNameOutput").innerHTML = "Invalid input somewhere";
-}
+    document.getElementById("OutputDOW").innerHTML = myDOW ;
+    document.getElementById("gender").innerHTML = gender ;
+    // an array of male names
+    var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    //an array of female names
+    var female = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    //an array of days of the week
+    var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"];
+    
+    if(myDOW === 0 && gender=== "Male"){
+        document.getElementById("akanNameOutput").innerHTML = male[0] ;
+    }else if(myDOW === 0 && gender === "Female"){
+        document.getElementById("akanNameOutput").innerHTML = female[0] ;
+    }else if(myDOW === 1 && gender=== "Male"){
+        document.getElementById("akanNameOutput").innerHTML = male[1] ;
+    }else if(myDOW === 1 && gender === "Female"){
+        document.getElementById("akanNameOutput").innerHTML = female[1] ;
+    }else if(myDOW === 2 && gender=== "Male"){
+        document.getElementById("akanNameOutput").innerHTML = male[2] ;
+    }else if(myDOW === 2 && gender === "Female"){
+        document.getElementById("akanNameOutput").innerHTML = female[2] ;
+    }else if(myDOW === 3 && gender=== "Male"){
+        document.getElementById("akanNameOutput").innerHTML = male[3] ;
+    }else if(myDOW === 3 && gender === "Female"){
+        document.getElementById("akanNameOutput").innerHTML = female[3] ;
+    }else if(myDOW === 4 && gender=== "Male"){
+        document.getElementById("akanNameOutput").innerHTML = male[4] ;
+    }else if(myDOW === 4 && gender === "Female"){
+        document.getElementById("akanNameOutput").innerHTML = female[4] ;
+    }else if(myDOW === 5 && gender=== "Male"){
+        document.getElementById("akanNameOutput").innerHTML = male[5] ;
+    }else if(myDOW === 5 && gender === "Female"){
+        document.getElementById("akanNameOutput").innerHTML = female[5] ;
+    }else if(myDOW === 6 && gender=== "Male"){
+        document.getElementById("akanNameOutput").innerHTML = male[6] ;
+    }else if(myDOW === 6 && gender === "Female"){
+        document.getElementById("akanNameOutput").innerHTML = female[6] ;
+    }else{
+        document.getElementById("akanNameOutput").innerHTML = "Invalid input somewhere";
+    }
 
 }
